d2002f4bb6e7513b662cd92e3516392f246e516b
ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>
2024-03-30 09:22:47 +0630

    Update start.py
    Signed-off-by: ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/FoRBIDDen_FeeLinG5/4"
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/seriousvs_version20"
         ),
         ],
     ]
